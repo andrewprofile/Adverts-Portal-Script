abd8c38b8e20470eb9a1cd766ce70141f026d20b
andrewprofile <andrkos287@student.polsl.pl>
2017-01-02 12:50:33 +0100

    Add foreach Users

--- src/Laftika/Controllers/HomeController.cs
+++ src/Laftika/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Laftika.DAL;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Laftika.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IUserRepository _userRepository;
+
+        public HomeController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
         public IActionResult Index()
         {
+            ViewBag.Users = _userRepository.GetAll();
+
             return View();
         }
 
